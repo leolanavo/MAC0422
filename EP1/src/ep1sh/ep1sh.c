0ee1b806352321227a3dbf887f9bb08f054061dd
Leonardo Lana <leolanavo@gmail.com>
2017-08-17 11:25:34 -0300

    Make the program last more than one command.
    Start building the string reading.

--- EP1/src/ep1sh/ep1sh.c
+++ EP1/src/ep1sh/ep1sh.c
@@ -1,32 +1,61 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <stdarg.h>
-#include <readline/readline.h>
+
 #include <readline/history.h>
+#include <readline/readline.h>
 
-void input_interface () {
+void process_cmd(char* cmd) {
+    /* "/bin/ping -c 10 www.google.com.br"; */
+    /* "/bin/usr/cal 2017"; */
+    /* "./ep1"; */
 
-    char dir[1024], *s;
-
-    s = getcwd (dir, sizeof(dir) - 1);
-
-    if (s == 0)
-        printf ("Error getting pwd: %s\n", dir);
- 
-    char* str = malloc(sizeof(dir) + 4);
-    sprintf(str, "[%s]$ ", dir);
+    int i;
+    int size = strlen(cmd);
     
-    char *line = readline (str);
-    if (line && *line)
-        add_history(line);
+    for (i = 0; i < size && cmd[i] != 32; i++);
+    
+    char* path = malloc(i--);
+    while(i >= 0) {
+        path[i] = cmd[i];
+        i--;
+    }
 
-    free(str);
-    free(line);
+    printf("cmd: %d\n", size);
+    printf("path: %lu\n", strlen(path));
+    printf("your path: %s\n", path);
+
+    free(path);
+    path = NULL;
 }
 
-int main () {
-  
-    input_interface();    
+void input_interface() {
+    char dir[1024], *s;
+
+    while (1) {
+        s = getcwd(dir, sizeof(dir) - 1);
+
+        if (s == 0)
+            printf("Error getting pwd: %s\n", dir);
+
+        char* str = malloc(sizeof(dir) + 4);
+        sprintf(str, "[%s]$ ", dir);
+
+        char* line = readline(str);
+        printf("%s\n", line);
+        if (line && *line) {
+            process_cmd(line);
+            add_history(line);
+        }
+
+        free(str);
+        free(line);
+    }
+}
+
+int main() {
+    input_interface();
     return 0;
 }
