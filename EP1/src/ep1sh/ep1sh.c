4eaa707db8519e610b600759dc37e06b7f998020
biamarou <biamarou@gmail.com>
2017-08-20 13:42:44 +0000

    Commands 'date' and 'chown' implemented

--- EP1/src/ep1sh/ep1sh.c
+++ EP1/src/ep1sh/ep1sh.c
@@ -2,33 +2,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <unistd.h>
+#include <sys/time.h>
+#include <grp.h>
 
 #include <readline/history.h>
 #include <readline/readline.h>
 
-void process_cmd(char* cmd) {
+void process_cmd(char *cmd, char *lc_dir) {
     /* "/bin/ping -c 10 www.google.com.br"; */
     /* "/bin/usr/cal 2017"; */
     /* "./ep1"; */
 
+    /*Get the first string of the command*/
     int i;
     int size = strlen(cmd);
     
     for (i = 0; i < size && cmd[i] != 32; i++);
+    char *path = calloc(i, sizeof(char));
     
-    char* path = malloc(i--);
-    while(i >= 0) {
+    for (i = i - 1; i >= 0; i--)
         path[i] = cmd[i];
-        i--;
+
+    printf("check path: %s\n", path);
+
+    /*Date command*/
+    if (strcmp(path,"date") == 0) { 
+        
+        struct tm *br = malloc(sizeof(struct tm));
+        time_t brtime = time(&brtime);
+
+        br = localtime_r (&brtime, br);
+        char *time = asctime(br);
+
+        int str_len = strlen(time);
+        char *str_time = calloc(str_len + 2, sizeof(char));
+        for (int j = 0; j < str_len - 6; j++)
+            str_time[j] = time[j];
+
+        printf("%s %s %d\n", str_time, br->tm_zone, br->tm_year + 1900);
+        
+        free(br);
+        free(str_time);
     }
 
-    printf("cmd: %d\n", size);
-    printf("path: %lu\n", strlen(path));
-    printf("your path: %s\n", path);
+    /*Chown command*/
+    else if (strcmp(path,"chown") == 0) {
+        
+        int sz_group, offset, tmp;
+        offset = 7; /*removing "chown :" from the counting*/
 
-    free(path);
-    path = NULL;
+        for (sz_group = offset; sz_group < size && cmd[sz_group] != 32; sz_group++);    
+        char *grp_name = calloc(sz_group - offset, sizeof(char));
+        
+        tmp = sz_group - 1;       
+        for (int j = sz_group - offset - 1; tmp >= offset; tmp--, j--)
+            grp_name[j] = cmd[tmp];
+
+        int sz_file = size - sz_group - 1;
+        char *file_name = calloc (sz_file, sizeof(char));
+        
+        tmp = size - 1;
+        for (int j = sz_file - 1; tmp >= size - sz_file; j--, tmp--)
+            file_name[j] = cmd[tmp];
+
+        char *path_file = calloc (size + sz_file, sizeof(char));
+    
+        struct group *grp = getgrnam (grp_name);
+        if (grp == NULL) printf("There is no such group named '%s'\n", grp_name);
+        else {
+            int ret_value = chown (file_name, (uid_t)-1, grp->gr_gid);
+            if (ret_value == -1) printf("chown: erro\n");
+        }
+    }
+
 }
 
 void input_interface() {
@@ -44,9 +92,9 @@ void input_interface() {
         sprintf(str, "[%s]$ ", dir);
 
         char* line = readline(str);
-        printf("%s\n", line);
+
         if (line && *line) {
-            process_cmd(line);
+            process_cmd(line, dir);
             add_history(line);
         }
 
