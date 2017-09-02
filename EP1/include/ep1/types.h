34ac7891c6dc77d12deb819c3195c8e12c9115a4
Leonardo Lana <leolanavo@gmail.com>
2017-09-02 19:11:57 -0300

    Add the rrqueue and node structs

--- EP1/include/ep1/types.h
+++ EP1/include/ep1/types.h
@@ -9,4 +9,19 @@ typedef struct {
     int times[4];
 } process;
 
+/* This structure has a process pointer to store the content 
+ * of a node in the queue and a next to store which node 
+ * is the next in the queue. */
+typedef struct cell {
+    process* p;
+    struct cell* next;
+} node;
+
+/* This structure has two node pointers to indicate in 
+ * which nodes the queue begins and ends. */
+typedef struct {
+    node* first;
+    node* last;
+} rrqueue;
+
 #endif
