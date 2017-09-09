130d276f8762f9e76e4dd9d906a93dd41aff31f7
Leonardo Lana <leolanavo@gmail.com>
2017-09-08 21:07:42 -0300

    Change the progress indicator

--- EP1/generator.py
+++ EP1/generator.py
@@ -10,8 +10,8 @@ def main():
             for i in range (1, 31):
                 trace_fl = "trace" + str(s) + "_" + str(i) + ".txt"
                 result_fl = "result" + str(s) + "_" + str(id) + "_"+ str(i) + ".txt"
-                print(trace_fl)
 
+                print(result_fl)
                 file_input = open(trace_fl, 'w')
                 file_output = open(result_fl, 'w')
                 file_output.close()
@@ -24,6 +24,5 @@ def main():
                     file_input.write(str(round(t0,1)) + " " + str(round(dt,1)) + " " + str(round(dead,1)) + " p" + str(i) + "\n")
 
                 file_input.close()
-
                 sp.run(["./bin/ep1", str(id), trace_fl, result_fl])
 main()
