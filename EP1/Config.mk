de0d0d38f0078a81b62916a477a9e258e6113480
Leonardo Lana <leolanavo@gmail.com>
2017-09-22 14:24:29 -0300

    Make the Makefile more friendly

---
+++ EP1/Config.mk
@@ -0,0 +1,18 @@
+# Configuration file for the makefile
+
+###################################################
+# BIN -> binaries                                 #
+# CFLAGS -> compilation flags                     #
+# LDFLAGS -> linker flags                         #
+# FINALDIR -> the name of the directory that will #
+#             be compressed in .tar.gz            #
+# TARF -> the name of the .tar.gz file            #
+#                                                 #
+###################################################
+
+BIN       := ep1 ep1sh
+TARF      := ep1-leonardo-beatriz
+FINALDIR  := ep1-leonardo-beatriz
+
+CFLAGS    := -Wall -Werror -Wextra -std=gnu11
+LDFLAGS   := -I $(INCDIR) -lm -lpthread -lreadline
