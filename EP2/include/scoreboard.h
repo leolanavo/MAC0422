5ac4635b11f488476ab1e1a83ec71b3aaaf95d66
Leonardo Lana <leolanavo@gmail.com>
2017-10-08 17:26:45 -0300

    Create the scoreboard files

---
+++ EP2/include/scoreboard.h
@@ -0,0 +1,8 @@
+#ifndef SCOREBOARD_H
+#define SCOREBOARD_H
+
+int compareScore(const void* a, const void* b);
+
+void print_scoreboard(race* r, bool verbose);
+
+#endif
---
+++ EP2/src/scoreboard.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "types.h"
+
+int compareScore(const void* a, const void* b) {
+    if ((**(cyclist**)a).score > (**(cyclist**)b).score) return -1;
+    if ((**(cyclist**)a).score == (**(cyclist**)b).score) return 0;
+    return 1;
+}
+
+void print_scoreboard(race* r, bool verbose) {
+    cyclist** caux = malloc(r->ncomp * sizeof(cyclist*));
+    for (uint i = 0; i < r->ncomp; i++)
+        caux[i] = r->comp[i];
+    qsort(caux, r->ncomp, sizeof(cyclist*), compareScore);
+
+    for (uint i = 0; i < r->ncomp; i++)
+        if (verbose)
+            printf("%u - %d - %u\n", i, caux[i]->id, caux[i]->score);
+        else
+            printf("%u - %d\n", i, caux[i]->id);
+}
+
