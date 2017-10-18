df0c6ef131b27a6147c3d1bdd6d49b00b880c38c
Leonardo Lana <leolanavo@gmail.com>
2017-10-18 20:26:40 -0200

    Initial commit for EP3

---
+++ EP3/Config.mk
@@ -0,0 +1,23 @@
+#################################################
+# Leonardo Lana Violin Oliveira - NUSP: 9793735 #
+# Beatriz Figuereido Marouelli  - NUSP: 9793652 #
+#################################################
+
+# Configuration file for the makefile
+
+###################################################
+# BIN -> binaries                                 #
+# CFLAGS -> compilation flags                     #
+# LDFLAGS -> linker flags                         #
+# FINALDIR -> the name of the directory that will #
+#             be compressed in .tar.gz            #
+# TARF -> the name of the .tar.gz file            #
+#                                                 #
+###################################################
+
+BIN       := ep2
+TARF      := ep2-leonardo-beatriz
+FINALDIR  := ep2-leonardo-beatriz
+
+CFLAGS    := -Wall -Werror -Wextra -std=gnu11
+LDFLAGS   := -I $(INCDIR) -lpthread
