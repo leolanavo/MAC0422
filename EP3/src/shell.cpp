e5a08d522d1fa86be6479a5ce41dfb3b62432094
Leonardo Lana <leolanavo@gmail.com>
2017-11-06 16:54:07 -0200

    Fix string spllinting

--- EP3/src/shell.cpp
+++ EP3/src/shell.cpp
@@ -1,8 +1,4 @@
-#include <fstream>
-#include <iostream>
-#include <string>
-
-using namespace std;
+#include "shell.h"
 
 #define NCMD 5
 typedef unsigned int uint;
@@ -26,24 +22,26 @@ int find_next_word(string str, int start) {
  * from the original string separated by blank space.
  */
 string* split_spaces(string str) {
-    int nspc = 0;
+    int nspc = 1;
 
     for (uint i = 0; i < str.length(); i++)
         if (isspace(str[i])) nspc++;
 
     string* parsed_str = new string[nspc];
 
-    int j;
+    int j = 0;
     nspc = 0;
+
     for (uint i = 0; i < str.length(); i++)
         if (isspace(str[i])) {
-            parsed_str[nspc] = str.substr(j, i - 1);
+            parsed_str[nspc] = str.substr(j, i);
             j = find_next_word(str, i);
             if (j == -1) break;
             i = j;
             nspc++;
         }
 
+    parsed_str[nspc] = str.substr(j, str.length());
     return parsed_str;
 }
 
@@ -60,31 +58,25 @@ void prompt() {
     int free_sp, page_sub, time;
 
     string cmds[NCMD] = { "carrega", "espaco", "substitui", "executa", "sai" };
-    int cmd_codes[NCMD];
-    for (int i = 0; i < NCMD; i++)
-        cmd_codes[i] = stoi(cmds[i]);
 
     while (true) {
-
-        cout << "[ep3]:" << endl;
+        cout << "[ep3]: ";
         getline(cin, cmd_line);
         string* cmd = split_spaces(cmd_line);
 
-        int code = stoi(cmd[0]);
-
-        if (code == cmd_codes[0])
+        if (cmd[0] == cmds[0])
             in_file.open(cmd[1]);
 
-        else if (code == cmd_codes[1])
-            free_sp = stoi(cmd_line.substr(7), NULL, 10);
+        else if (cmd[0] == cmds[1])
+            free_sp = stoi(cmd[1]);
 
-        else if (code == cmd_codes[2])
-            page_sub = stoi(cmd_line.substr(10), NULL, 10);
+        else if (cmd[0] == cmds[2])
+            page_sub = stoi(cmd[1]);
 
-        else if (code == cmd_codes[3])
-            time = stoi(cmd_line.substr(8), NULL, 10);
+        else if (cmd[0] == cmds[3])
+            time = stoi(cmd[1]);
 
-        else if (code == cmd_codes[4])
+        else if (cmd[0] == cmds[4])
             exit(0);
 
         else
