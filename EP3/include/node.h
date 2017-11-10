9aead2216218c8a631eff6b407f654bd932770bd
Leonardo Lana <leolanavo@gmail.com>
2017-11-09 23:35:53 -0200

    Fix LinkedList template

--- EP3/include/node.h
+++ EP3/include/node.h
@@ -13,4 +13,22 @@ public:
     T get_data();
 };
 
+using namespace std;
+
+template <class T>
+Node<T>::Node(T data) :
+    data(data), next(NULL), prev(NULL)
+{
+}
+
+template <class T>
+void Node<T>::set_data(T data) {
+    this->data = data;
+}
+
+template <class T>
+T Node<T>::get_data() {
+    return data;
+}
+
 #endif
